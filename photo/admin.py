e00dce7bd26361f8b72e61f3e469e30f40acffd5
sa46lll <9812smh@naver.com>
2021-09-09 23:26:09 +0900

    [Feat] view, list.html 생성

--- photo/admin.py
+++ photo/admin.py
@@ -5,7 +5,7 @@ from photo.models import Photo
 
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ['id', 'author', 'created', 'updated']
-    raw_id_fields = ['author']
+    raw_id_fields = ['author']  # ForeignKey 를 검색기능으로 선택
     list_filter = ['created', 'updated', 'author']
     search_fields = ['text', 'created'] # 검색할 필드 선택(foreignKey 제외)
     ordering = ['updated', '-created']
--- photo/views.py
+++ photo/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import generic
 
-# Create your views here.
+from photo.models import Photo
+
+
+def photo_list(request):
+    photos = Photo.objects.all()
+    return render(request, 'photo/list.html', {'photos': photos})  # photos 라는 템플릿 변수를 같이 전달
+
+
+class PhotoUploadView(generic.CreateView):
+    model = Photo
+    fields = ['photo', 'text']
+    template_name = 'photo/upload.html'  # 실제 사용할 템플릿 설정
+
+
+def form_valid(self, form):  # 업로드를 끝낸 후 이동할 페이지 호출
+    form.instance_id = self.request.user.id  # 작성자는 현재 로그인 한 사용자로 설정
+    if form.is_valid():  # 입력된 값들 검증
+        form.instance.save()  # 데이터베이스 저장
+        return redirect('/')  # 메인페이지 이동
+    else:
+        return self.render_to_response({'form': form})  # 이상이 있다면 작성된 내용 그대로 작성 페이지에 표시
+
+
+class PhotoDeleteView(generic.DeleteView):
+    model = Photo
+    success_url = '/'
+    template_name = 'photo/delete.html'
+
+
+class PhotoUpdateView(generic.UpdateView):
+    model = Photo
+    fields = ['photo', 'text']
+    template_name = 'photo/update.html'
+
